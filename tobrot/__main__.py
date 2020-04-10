c698ccbd0da21836f819442f14c25606af4baf2a
Muhammad Nur Fauzan <60289157+NyaanChan@users.noreply.github.com>
2020-04-11 05:49:30 +0800

    Change command leech to download

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -54,7 +54,7 @@ if __name__ == "__main__" :
     #
     incoming_message_handler = MessageHandler(
         incoming_message_f,
-        filters=Filters.command(["leech"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["download"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_message_handler)
     #
